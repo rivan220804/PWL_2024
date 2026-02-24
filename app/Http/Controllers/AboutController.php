50578c615b6344a71fdd664b7a599edbc9f22740
rivan220804 <fadillahrivan25@gmail.com>
2026-02-24 14:53:43 +0700

    commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return "Rivan Fahlul Fadillah - 244107020239";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::get('/hello', [PageController::class, 'hello']);
+
+Route::get('/world', function () {
+    return 'World';
 });
+
+Route::get('/about', AboutController::class);
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/posts/{postId}/comments/{commentId}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ' Komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+'index', 'show'
+]);
+
